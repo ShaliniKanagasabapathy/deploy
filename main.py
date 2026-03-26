25007d233f1355aaa4f2e4225ce221851f985f5d
K.Shalini <148876247+ShaliniKanagasabapathy@users.noreply.github.com>
2026-03-26 13:12:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.title("My First Streamlit App")
+st.write("Hello Shalini 👋")
+st.header("header")
+st.subheader("sub header")
+st.text("text")
+st.markdown("markdown")
+st.caption("caption")
+import pandas as pd
+df=pd.DataFrame({"name":["a","b","c"],"mark":[12,14,16]})
+st.write(df)
+name=st.text_input("enter your name")
+age=st.number_input("enter age",min_value=10,max_value=60)
+rating=st.slider("Enter your rating",0,50,5)
+colour=st.selectbox("select colour",["red","blue","green"])
+st.write(colour)
+if st.checkbox("show message"):
+    st.write("hi how are you i like",colour)
+
+ch=st.radio("pick one",["idly","dosa"])
+from PIL import Image
+IMG=Image.open(r"C:\Users\shale\Desktop\Python\dashboard.jpg")
+st.image(IMG,caption="my dashboard")
+st.sidebar.title("visual studio")
+with st.expander("see more"):
+    st.write("hi")
+
+
